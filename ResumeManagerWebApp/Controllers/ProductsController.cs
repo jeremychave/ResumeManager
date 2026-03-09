e44b5917a5913d2e069bdee7dcbeca3e6af80f1c
Jérémy CHAVE <chave.j@hotmail.fr>
2026-03-09 16:06:20 -0700

    add ProductModel to webApp

---
+++ ResumeManagerWebApp/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResumeManagerWebApp.Services;
+
+namespace ResumeManagerWebApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ProductApiService _apiService;
+
+        public ProductsController(ProductApiService apiService)
+        {
+            _apiService = apiService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _apiService.GetProductsAsync();
+            return View(products);
+        }
+    }
+}
--- ResumeManagerWebApp/Program.cs
+++ ResumeManagerWebApp/Program.cs
@@ -1,6 +1,13 @@
+using ResumeManagerWebApp.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient<ProductApiService>(client =>
+{
+    client.BaseAddress = new Uri("http://localhost:5000/");
+});
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
 
 app.Run();
