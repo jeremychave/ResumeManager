cf6d4ab130fafac780d6e62876966648c983cbca
Jérémy CHAVE <chave.j@hotmail.fr>
2026-03-09 18:25:08 -0700

    add API

---
+++ ResumeManagerWebApi/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using ResumeManagerWebApi.Models;
+
+namespace ResumeManagerWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Laptop", Price = 999.99m },
+            new Product { Id = 2, Name = "Mouse", Price = 19.99m },
+            new Product { Id = 3, Name = "Keyboard", Price = 49.99m }
+        };
+
+            return Ok(products);
+        }
+    }
+}
--- ResumeManagerWebApp/Program.cs
+++ ResumeManagerWebApp/Program.cs
@@ -2,11 +2,15 @@ using ResumeManagerWebApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+
+var resumeManagerUrl = builder.Configuration["ApiSettings:ResumeManagerWebApi"];
 builder.Services.AddHttpClient<ProductApiService>(client =>
 {
-    client.BaseAddress = new Uri("http://localhost:5000/");
+    client.BaseAddress = new Uri(resumeManagerUrl);
 });
 var app = builder.Build();
+app.UseStaticFiles();
+app.MapDefaultControllerRoute();
 
 //app.MapGet("/", () => "Hello World!");
 
