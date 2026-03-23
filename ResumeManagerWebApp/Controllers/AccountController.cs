864f91697db4b638b605b29d004a5607fa825a64
Jérémy CHAVE <chave.j@hotmail.fr>
2026-03-23 16:25:52 -0700

    login button

---
+++ ResumeManagerWebApp/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+using ResumeManagerWebApp.Controllers;
+
+public class AccountController : Controller
+{
+    [HttpGet]
+    public async Task<IActionResult> Login()
+    {
+        if (User.Identity != null && User.Identity.IsAuthenticated)
+        {
+            return RedirectToAction(nameof(DocumentsController.Index), "Document");
+        }
+
+        var redirectUrl = Url.Action(nameof(DocumentsController.Index), "Document", null, Request.Scheme);
+
+        return Challenge(
+            new AuthenticationProperties { RedirectUri = redirectUrl },
+            OpenIdConnectDefaults.AuthenticationScheme);
+    }
+
+    //public IActionResult Logout()
+    //{
+    //    return SignOut(
+    //        new AuthenticationProperties { RedirectUri = "/" },
+    //        CookieAuthenticationDefaults.AuthenticationScheme,
+    //        OpenIdConnectDefaults.AuthenticationScheme
+    //    );
+    //}
+
+    //public IActionResult AccessDenied()
+    //{
+    //    return View();
+    //}
+}
--- ResumeManagerWebApp/Program.cs
+++ ResumeManagerWebApp/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using ResumeManagerWebApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +11,47 @@ builder.Services.AddHttpClient<DocumentApiService>(client =>
 {
     client.BaseAddress = new Uri(resumeManagerUrl);
 });
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+})
+.AddCookie()
+.AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+{
+    var config = builder.Configuration.GetSection("AzureAdB2C");
+
+    options.Authority = $"{config["Instance"]}/{config["Domain"]}/{config["SignUpSignInPolicyId"]}/v2.0";
+    options.ClientId = config["ClientId"];
+    options.CallbackPath = config["CallbackPath"];
+    options.SignedOutCallbackPath = config["SignedOutCallbackPath"];
+    options.ResponseType = "id_token";
+    options.SaveTokens = true;
+
+    options.Scope.Add("openid");
+    options.Scope.Add("profile");
+
+    options.Events = new OpenIdConnectEvents
+    {
+        OnRemoteFailure = context =>
+        {
+            if (context.Failure.Message.Contains("AADB2C90118"))
+            {
+                context.Response.Redirect("/Account/ResetPassword");
+                context.HandleResponse();
+            }
+            return Task.CompletedTask;
+        }
+    };
+});
+
+//builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+//    .AddOpenIdConnect(options =>
+//    {
+//        builder.Configuration.Bind("AzureAdB2C", options);
+//        options.TokenValidationParameters.NameClaimType = "name";
+//    });
 
 var app = builder.Build();
 app.UseStaticFiles();
