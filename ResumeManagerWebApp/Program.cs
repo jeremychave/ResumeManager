19c9c7ce36498cce650e8d25ee64208724667559
Jérémy CHAVE <chave.j@hotmail.fr>
2026-03-24 16:32:12 -0700

    add login to web app

--- ResumeManagerWebApp/Program.cs
+++ ResumeManagerWebApp/Program.cs
@@ -1,5 +1,5 @@
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Identity.Web;
 using ResumeManagerWebApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,58 +12,18 @@ builder.Services.AddHttpClient<DocumentApiService>(client =>
     client.BaseAddress = new Uri(resumeManagerUrl);
 });
 
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
-})
-.AddCookie()
-.AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
-{
-    var config = builder.Configuration.GetSection("AzureAdB2C");
-
-    options.Authority = $"{config["Instance"]}/{config["Domain"]}/{config["SignUpSignInPolicyId"]}/v2.0";
-    options.ClientId = config["ClientId"];
-    options.CallbackPath = config["CallbackPath"];
-    options.SignedOutCallbackPath = config["SignedOutCallbackPath"];
-    options.ResponseType = "id_token";
-    options.SaveTokens = true;
-
-    options.Scope.Add("openid");
-    options.Scope.Add("profile");
-
-    options.Events = new OpenIdConnectEvents
-    {
-        OnRemoteFailure = context =>
-        {
-            if (context.Failure.Message.Contains("AADB2C90118"))
-            {
-                context.Response.Redirect("/Account/ResetPassword");
-                context.HandleResponse();
-            }
-            return Task.CompletedTask;
-        }
-    };
-});
-
-//builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-//    .AddOpenIdConnect(options =>
-//    {
-//        builder.Configuration.Bind("AzureAdB2C", options);
-//        options.TokenValidationParameters.NameClaimType = "name";
-//    });
+builder.Services
+    .AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(builder.Configuration);
 
 var app = builder.Build();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseRouting();
+
 app.UseAuthentication();
 app.UseAuthorization();
-app.UseStaticFiles();
-app.UseEndpoints(app =>
-{
-    app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}");
-});
-//app.MapDefaultControllerRoute();
+
+app.MapDefaultControllerRoute();
 
 app.Run();
