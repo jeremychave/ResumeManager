a90be4970ad06945b1d9a5872d35383eccacbdb3
Jérémy CHAVE <chave.j@hotmail.fr>
2026-03-26 14:57:40 -0700

    add secret in keyVault

--- ResumeManagerWebApi/Program.cs
+++ ResumeManagerWebApi/Program.cs
@@ -1,7 +1,14 @@
+using Azure.Identity;
 using ResumeManagerWebApi.Repositories;
 using ResumeManagerWebApi.Services.Documents;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var keyVaultUrl = builder.Configuration["KeyVaultUrl"];
+builder.Configuration.AddAzureKeyVault(
+    new Uri(keyVaultUrl),
+    new DefaultAzureCredential()
+);
 
 // Add services to the container.
 
--- ResumeManagerWebApp/Program.cs
+++ ResumeManagerWebApp/Program.cs
@@ -1,3 +1,4 @@
+using Azure.Identity;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Identity.Web;
 using ResumeManagerWebApp.Services;
@@ -15,6 +16,12 @@ builder.Services.AddHttpClient<DocumentApiService>(client =>
 builder.Services
     .AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApp(builder.Configuration);
+
+var keyVaultUrl = builder.Configuration["KeyVaultUrl"];
+builder.Configuration.AddAzureKeyVault(
+    new Uri(keyVaultUrl),
+    new DefaultAzureCredential()
+);
 
 var app = builder.Build();
 app.UseHttpsRedirection();
