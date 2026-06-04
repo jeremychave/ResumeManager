9fb77f41d67a06ea5022a0c4d2b2c264e913a00e
Jérémy CHAVE <chave.j@hotmail.fr>
2026-06-04 15:00:22 -0700

    add AccessTokenInterceptor

---
+++ ResumeManagerWebApi/Data/AccessTokenInterceptor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System.Data.Common;
+
+public class AccessTokenInterceptor : DbConnectionInterceptor
+{
+    public override InterceptionResult ConnectionOpening(
+        DbConnection connection,
+        ConnectionEventData eventData,
+        InterceptionResult result)
+    {
+        var token = Environment.GetEnvironmentVariable("SQL_ACCESS_TOKEN");
+
+        if (!string.IsNullOrEmpty(token) && connection is SqlConnection sqlConnection)
+        {
+            sqlConnection.AccessToken = token;
+        }
+
+        return base.ConnectionOpening(connection, eventData, result);
+    }
+}
--- ResumeManagerWebApi/Program.cs
+++ ResumeManagerWebApi/Program.cs
@@ -7,8 +7,10 @@ using ResumeManagerWebApi.Services.User;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<ResumeManagerDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ResumeManagerDb")));
+builder.Services.AddDbContext<ResumeManagerDbContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ResumeManagerDb"));
+    options.AddInterceptors(new AccessTokenInterceptor());
+});
 
 builder.Services.AddControllers();
 builder.Services.AddScoped<IUserService, UserService>();
