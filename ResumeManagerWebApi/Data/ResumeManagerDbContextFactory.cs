7713c80840d172a1c57fdf70cf5de845f7357446
Jérémy CHAVE <chave.j@hotmail.fr>
2026-06-08 14:51:18 -0700

    add ResumeManagerDbContextFactory

---
+++ ResumeManagerWebApi/Data/ResumeManagerDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using ResumeManagerWebApi.Data;
+
+public class ResumeManagerDbContextFactory : IDesignTimeDbContextFactory<ResumeManagerDbContext>
+{
+    public ResumeManagerDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ResumeManagerDbContext>();
+
+        var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__ResumeManagerDb");
+
+        optionsBuilder.UseSqlServer(connectionString);
+        optionsBuilder.AddInterceptors(new AccessTokenInterceptor());
+
+        return new ResumeManagerDbContext(optionsBuilder.Options);
+    }
+}
